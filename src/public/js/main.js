650194b0c65f33e088e16da2ff5e93245059549d
Sohyun <park.sohyun0430@gmail.com>
2021-10-30 17:40:44 +0900

    login

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,18 @@
-const btnSignUp = document.querySelector("#btnSignUp");
 
+//loginBtn
+const btnSignIn = document.querySelector("#btnSignIn");
+btnSignIn.addEventListener("click",login);
+
+function login()
+{
+    
+}
+
+
+const btnSignUp = document.querySelector("#btnSignUp");
 btnSignUp.addEventListener("click", SignUp);
+
+
 
 function SignUp(event){
     event.preventDefault();
