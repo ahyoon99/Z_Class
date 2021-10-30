3ff7fb1c2dc454bead7524c1439458c027036f20
Sohyun <park.sohyun0430@gmail.com>
2021-10-30 18:47:31 +0900

    login update

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,9 +3,20 @@
 const btnSignIn = document.querySelector("#btnSignIn");
 btnSignIn.addEventListener("click",login);
 
-function login()
+async function login()
 {
-    
+    var id=document.getElementById("loginId").value;
+    var pwd=document.getElementById("loginPassword").value;
+    if(id==""||pwd=="")
+    {
+       alert("아이디 또는 비밀번호를 입력해주세요.");
+    }
+    else{
+        
+       alert("아이디:"+id+"pwd:"+pwd);
+       //이후에 id, pwd 디비랑 비교해서 있는지 확인 없으면 회원가입 해주세요, 있으면 강의실로 입장.
+    }
+
 }
 
 
